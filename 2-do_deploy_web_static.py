7b830de7b7bc839004303397b02c7257ea0927a1
nickfff-dev <hunchographics@gmail.com>
2023-12-12 02:32:14 +0300

    combined

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,7 +2,7 @@
 """ Fabric script that distributes an archive to your web servers,
 using the function do_deploy.
 """
-from fabric.api import env, put, run
+from fabric.api import *
 from os.path import exists
 
 env.hosts = ['52.3.245.73', '18.204.20.55']
@@ -21,13 +21,13 @@ def do_deploy(archive_path):
         file_name = archive_path.split('/')[-1]
         no_ext = file_name.split('.')[0]
         path = '/data/web_static/releases/'
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, no_ext))
-        run('rm /tmp/{}'.format(file_name))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        run('sudo mkdir -p {}{}/'.format(path, no_ext))
+        run('sudo tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, no_ext))
+        run('sudo rm /tmp/{}'.format(file_name))
+        run('sudo mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('sudo rm -rf {}{}/web_static'.format(path, no_ext))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except Exception:
         return False
