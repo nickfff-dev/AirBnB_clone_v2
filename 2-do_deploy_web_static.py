44730312cf0b436ca1e16f76243bf832661622f5
nickfff-dev <hunchographics@gmail.com>
2023-12-12 02:49:16 +0300

    combined

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,7 +2,7 @@
 """ Fabric script that distributes an archive to your web servers,
 using the function do_deploy.
 """
-from fabric.api import *
+from fabric.api import env, put, run
 from os.path import exists
 
 env.hosts = ['52.3.245.73', '18.204.20.55']
@@ -11,23 +11,45 @@ env.key_filename = '~/.ssh/school'
 
 
 def do_deploy(archive_path):
-    """ Distributes an archive to your web servers,
-    using the function do_deploy.
     """
+    Distributes an archive to web servers
+    """
+
+    # Return False if the file at the path archive_path doesn’t exist
     if not exists(archive_path):
         return False
+
     try:
-        put(archive_path, '/tmp/')
-        file_name = archive_path.split('/')[-1]
-        no_ext = file_name.split('.')[0]
-        path = '/data/web_static/releases/'
-        run('sudo mkdir -p {}{}/'.format(path, no_ext))
-        run('sudo tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, no_ext))
-        run('sudo rm /tmp/{}'.format(file_name))
-        run('sudo mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('sudo rm -rf {}{}/web_static'.format(path, no_ext))
-        run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, "/tmp/")
+
+        # Uncompress the archive to the folder
+        # /data/web_static/releases/<archive filename without extension>
+        # on the web server
+        file_name = archive_path.split("/")[-1]
+        no_ext, ext = file_name.split(".")
+        run("mkdir -p /data/web_static/releases/{}/".format(no_ext))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(file_name, no_ext))
+
+        # Delete the archive from the web server
+        run("rm /tmp/{}".format(file_name))
+
+        # Move the files
+        run("mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/".format(no_ext, no_ext))
+
+        # Delete the symbolic link /data/web_static/current from
+        # the web server
+        run("rm -rf /data/web_static/releases/{}/web_static"
+            .format(no_ext))
+        run("rm -rf /data/web_static/current")
+
+        # Create a new the symbolic link /data/web_static/current
+        # on the web server, linked to the new version of your code
+        # (/data/web_static/releases/<archive filename without extension>)
+        run("ln -s /data/web_static/releases/{}/ \
+            /data/web_static/current".format(no_ext))
         return True
-    except Exception:
+    except:
         return False
