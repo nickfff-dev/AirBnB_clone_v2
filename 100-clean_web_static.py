552e049ee823bf28b97c716552803a10a04a8745
nickfff-dev <hunchographics@gmail.com>
2023-12-12 03:37:38 +0300

    puppet

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -2,7 +2,7 @@
 """
 Fabric script that cleans up old versions of the web_static folder
 """
-from fabric.api import local, env, run, lcd, cd
+from fabric.api import *
 import os
 
 env.hosts = ['52.3.245.73', '18.204.20.55']
@@ -15,15 +15,15 @@ def do_clean(number=0):
     number = 1 if int(number) == 0 else int(number)
 
     # Delete all unnecessary archives in the versions folder
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for _ in range(number)]
     with lcd("versions"):
-        archives = sorted(os.listdir("versions"))
-        [archives.pop() for _ in range(number)]
         [local("rm ./{}".format(a)) for a in archives]
 
     # Delete all unnecessary archives in the
     # /data/web_static/releases folder of both of your web servers
     with cd("/data/web_static/releases"):
-        archives = sorted(run("ls -tr").split())
+        archives = run("ls -tr").split()
         archives = [a for a in archives if "web_static_" in a]
         [archives.pop() for _ in range(number)]
         [run("rm -rf ./{}".format(a)) for a in archives]
