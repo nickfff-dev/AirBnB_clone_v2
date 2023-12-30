24d9b92d43b3acf2ba7e24b132a6484f2fd582d9
nickfff-dev <hunchographics@gmail.com>
2023-12-31 00:12:35 +0300

    index

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+
+
 """ This module defines a flask basic app """
 from flask import Flask
-
-
 app = Flask(__name__)
+
+
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """ Method for simple server """
