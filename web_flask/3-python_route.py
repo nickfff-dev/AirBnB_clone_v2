69cf6c98978553a607b76236937ad3307874f47b
nickfff-dev <hunchographics@gmail.com>
2023-12-31 01:10:19 +0300

    python

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" This module defines a flask basic app """
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """ Method for simple server """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hnbnb():
+    """ hollas HBNB! """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def cRoute(text):
+    """ the c route a dymanic path"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def pRoute(text='is cool'):
+    """ the python route a dymanic path"""
+    return 'Python ' + text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
